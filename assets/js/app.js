0675c80b9eb8791a992f1dc8625108622f816447
Maria Ratino <mlratino@gmail.com>
2019-09-17 16:34:55 -0400

    Add files via upload
    I have added gif popouts, and I can't find the bug on the function that presents the loser images.  The script is showing on the right answer response.  I will continue to work on it, but I wanted to turn in what I have built line by line with the tutor and my resources.
    
    I am also planning to pull in the ajax and Api work that we are working on.

---
+++ assets/js/app.js
@@ -0,0 +1,171 @@
+// console.log( quizQuestions);
+// Initial values
+let counter = 10;
+let currentQuestion = 0;
+let score = 0;
+let lost = 0;
+let timer;
+
+// function getGiphy(){
+//  var queryUrl ="http://api.giphy.com/v1/gifs/search?q=parksandrec&api_key=AqeRwZk1AJ9TEai6gddvo5MTBBWNZ6NO";
+ 
+//  $.ajax({
+//     url: queryUrl,
+//     type: "get"
+//    }).then(function(response) {  
+
+// var results = response.data
+
+// console.log(results);
+//    });   
+// }
+
+
+
+
+function nextQuestion() {
+    const isQuestionOver = (quizQuestions.length -1) === currentQuestion;
+    if (isQuestionOver) {
+console.log('Game Over!');
+    displayResult();
+    }else {
+            currentQuestion++;
+            loadQuestion();
+    }
+}
+
+
+// Timer
+function timeUp(){
+    clearInterval(timer);
+    lost++;
+    preloadImage('lost');
+    setTimeout(nextQuestion, 3 * 1000);
+}
+
+function countDown() {
+    counter--;
+    $('#time').html('Timer:' + counter);
+if (counter === 0){
+    timeUp();
+}
+}
+// Display question and choices to the browswer
+function loadQuestion() {
+    counter = 10;
+    timer = setInterval(countDown,1000);
+
+    const question = quizQuestions[currentQuestion].question;
+    const choices = quizQuestions[currentQuestion].choices;
+
+$('#time').html('Timer:' + counter);
+$('#game').html(`
+    <h4>${question}<h4/>
+    ${loadChoices(choices)}
+    ${loadRemainingQuestion()}
+    `);
+}    
+
+function loadChoices(choices){
+    let result = '';
+    
+    for (let i = 0; i < choices.length; i++) {
+        result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
+    }   
+
+return result;
+}
+$(document).on('click', '.choice', function() {
+    clearInterval(timer);
+        
+    const selectedAnswer= $(this).attr('data-answer');   
+    const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+    
+        if (correctAnswer === selectedAnswer){
+
+            score++;
+            console.log("April Ludgate");
+            preloadImage('win');          
+            setTimeout(nextQuestion, 3*1000);
+    }
+    else{
+            lost++;   
+            console.log("Jerry Gingrinch");
+            preloadImage('lost');          
+            setTimeout(nextQuestion, 3*1000);
+    }
+ console.log('hi', selectedAnswer);
+});
+
+function displayResult(){
+    const result =`
+    <p>You answered ${score} questions correctly!</p>
+    <p>You missed ${lost} of the questions!</p>
+    <p>Total questions ${quizQuestions.length} </p>
+    <button class= "btn btn-lg btn-primary" id="reset">Play Again!</button>
+    `;
+    $('#game').html(result);
+}
+
+$(document).on('click','#reset', function() {
+   
+    counter = 10;
+    currentQuestion = 0;
+    score = 0;
+    lost = 0;
+    timer= null;
+    
+    loadQuestion();
+
+});
+    function loadRemainingQuestion() {
+        const remainingQuestion = quizQuestions.length-(currentQuestion +1);
+        const totalQuestion = quizQuestions.length;
+        return `Remaining Questions: ${remainingQuestion}/${totalQuestion}`;
+}
+    function randomImage(images) {
+        const random = Math.floor(Math.random() * images.length);
+        const randomImage = images[random];
+        return randomImage;
+} 
+
+
+    function preloadImage(status) {
+            const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+
+if(status==='win') {
+    $('#game').html(`
+    <p> class="preload-image">Treat Yo Self!</p>
+    <p> class="preload-image">Correct answer is <b>${correctAnswer}</b></p>
+    <img src ="${randomImage(winningImages)}"/>
+    `);
+} else{
+$('#game').html(`
+    <p> class="preload-image">Knope the correct answer was <b>${correctAnswer}</b></p>
+    <p> class="preload-image">You could still work for the government.</p>
+    <img src ="${randomImage(lostImages)}/>
+    
+    `);
+    console.log("Lil'Sebastian",randomImage);
+}
+}
+$('#start').click(function() {
+$('#start').remove();
+$('#time').html(counter);
+loadQuestion();
+
+});;
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+const quizQuestions = [
+{
+    question: "What is the name of Pawnee's rival city?",
+    choices:["Eastlane","Allenton","Eagleton","Haverford"],
+    correctAnswer: "Eagleton"
+},
+{
+    question: "Leslie has a longstanding crush on what man?",
+    choices:["Cory Booker","Pete Seger","Joe Biden","Mark Brendanawicz"],
+    correctAnswer: "Joe Biden"
+},
+{
+    question: "Which of the following has NOT happened to the Sullivan Street Pit?",
+    choices:["Blown up", "It's been a community gardern.","It's harbored a secret marijuana plant.","It's been filled."],
+    correctAnswer: "Blown up"
+},
+{
+    question: "Who did Louis CK play on the show?",
+    choices:["himself","Police officer Dave Sanderson","State Auditor", "Mark Henderson", "City board member Steve Jogeson"],
+    correctAnswer: "Police officer Dave Sanderson"
+},
+{
+    question: "Which man did Ann Perkins never date?",
+    choices:["Cory Booker","Tom Haverford","Chris Traeger","Mark Brendanawicz"],
+    correctAnswer: "Cory Booker"
+}
+];
+
+const winningImages= [
+'./images/andyHurrayFace.gif',
+'./images/dancingChris.gif',
+'./images/dancingRon.gif',
+'./images/monkeyAndy.gif',
+'./images/explodingConfetti.gif',
+'./images/leslieDancing.gif',
+'./images/monaLisa.gif',
+'./images/tomSmiling.gif',
+];
+
+const lostImages=[
+'./images/beenJammed.gif',
+'./images/epicFail.gif',
+'./images/whoAreU.gif',   
+'./images/benRaspberries.gif',
+'./images/jeanRalphioTheWorst.gif',
+'./images/chrisSick.gif',
+'./images/jerryPie.gif',
+'./images/leslieNope.gif',
+];
+
+
+
